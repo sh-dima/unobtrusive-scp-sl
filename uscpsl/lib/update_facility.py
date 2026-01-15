ff3f981dbae37b9b162f83d9e85cf095a8744b61
Дима Ш. <dima.o.sh@proton.me>
2026-01-15 21:17:17 +0000

    Update information keybind text
    The information emoji does not render in SCP:SL.

--- uscpsl/lib/update_facility.py
+++ uscpsl/lib/update_facility.py
@@ -2,6 +2,6 @@ def update_facility(facility: str, names: dict[str, str]) -> str:
 	lines = facility.splitlines()
 
 	lines[32] = "➜] [escape_minutes]:[escape_seconds]"
-	lines[34] = "ℹ️ <color=yellow><b>F1</b></color>"
+	lines[34] = "[i] <color=yellow><b>F1</b></color>"
 
 	return "\n".join(lines)
