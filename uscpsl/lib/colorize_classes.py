10a812c1e8bea852ca85921734d9d66855e94c09
Дима Ш. <dima.o.sh@proton.me>
2026-01-21 20:41:51 +0000

    Add color to class D and facility guard names

--- uscpsl/lib/colorize_classes.py
+++ uscpsl/lib/colorize_classes.py
@@ -10,4 +10,8 @@ def colorize_classes(classes: str, names: dict[str, str]) -> str:
 			continue
 		lines[scp] = f"<color=#F00>{lines[scp]}</color>"
 
+	# Source for class colors: https://en.scpslgame.com/index.php?title=Data/Color&action=edit
+	lines[1] = f"<color=#FF8E00>{lines[1]}</color>" # Class D
+	lines[15] = f"<color=#5B6370>{lines[15]}</color>" # Facility guards
+
 	return "\n".join(lines)
